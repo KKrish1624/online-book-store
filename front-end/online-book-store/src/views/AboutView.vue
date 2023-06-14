f6d33654ab0887e831bb25a768b8a79b989bc154
Krish Athreyan <krishathreyan@192.168.1.3>
2023-06-14 12:07:01 +0530

    Adding Front End Files
    Removed the front-end folder as a submodule, deleted its entry and adding it again.

---
+++ front-end/online-book-store/src/views/AboutView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="about">
+    <h3 v-text="render"></h3>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      render: "",
+    };
+  },
+  methods: {
+    getResponse() {
+      const path = "http://127.0.0.1:5000/second";
+      axios.get(path).then((res) => {
+        console.log(res.data);
+        this.render = res.data;
+      });
+    },
+  },
+  created() {
+    this.getResponse();
+  },
+};
+</script>
---
+++ front-end/online-book-store/src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/cover.png" />
+    <h3>Welcome to Your Vue.js App</h3>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+</script>
